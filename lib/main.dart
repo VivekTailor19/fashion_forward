4f8b7ec4ab14b8897010467e2d7f3ca96a127ffc
VIVEK <tailorvivekgec19@gmail.com>
2023-07-28 16:27:01 +0530

    fashion_forward

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:fashion_forward/screens/01%20splash/splashScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    Sizer(
+      builder: (context, orientation, deviceType) =>  GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes:{
+          "/":(p0) => SplashScreen(),
+        },
+      ),
+    ),
+  );
+}
---
+++ lib/screens/01 splash/splashScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(),
+    );
+  }
+}
